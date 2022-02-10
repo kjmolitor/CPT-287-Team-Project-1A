2f716f699a3d1289ac1bb553e672a498235b88e7
Tyan206 <81524960+Tyan206@users.noreply.github.com>
2022-02-10 17:19:15 -0600

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,62 @@
+package project_1a;
+
+import java.util.Date;
+
+public class Movie {
+	private Date releaseDate;
+	private String name;
+	private String description;
+	private Date receiveDate;
+	private String status;
+	
+	
+	public Movie() {}
+	
+	public Movie(Date releaseDate, String name, String description, Date receiveDate, String status) {
+		this.releaseDate = releaseDate;
+		this.name = name;
+		this.description = description;
+		this.receiveDate = receiveDate;
+		this.status = status;
+	}
+	
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public Date getReceiveDate() {
+		return receiveDate;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public void setReceiveDate(Date receiveDate) {
+		this.receiveDate = receiveDate;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
